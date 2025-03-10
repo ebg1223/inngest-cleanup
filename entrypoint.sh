49649d776d2d2de72a0dd8a7a33835b95c26933d
ebg1223 <egreenfeld@azswa.org>
2025-03-09 21:51:32 -0700

    fix entry

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,11 @@ echo "Current environment:"
 echo "PATH=$PATH"
 echo "CRON_SCHEDULE=$CRON_SCHEDULE"
 
+# Export all environment variables to a file
+printenv > /app/env.sh
+
 # Use absolute paths for commands
-echo "${CRON_SCHEDULE} /usr/local/bin/python /app/cleanup_events.py && date > /app/last_success" > /app/crontab
+echo "${CRON_SCHEDULE} . /app/env.sh && /usr/local/bin/python /app/cleanup_events.py && date > /app/last_success" > /app/crontab
 
 # Print the crontab file for debugging
 echo "Contents of crontab file:"
